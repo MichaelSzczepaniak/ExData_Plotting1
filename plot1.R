e41044e4fd311890cf395f3e1b405b2c179cfb6a
MichaelSzczepaniak <mszczepaniak@pobox.com>
2015-08-04 16:11:16 -0600

    add GAP hist and code to generate it: 1st of 4 plots

--- plot1.R
+++ plot1.R
@@ -1,16 +1,28 @@
-# Read just the data from the data 2007-02-01 and 2007-02-02
-fileName <- "household_power_consumption.txt"
-colClasses <- c("character", "character", rep("numeric", 7))
-con <- file(fileName)
-open(con)
-headers <- unlist(read.table(con, sep = ";", nrows = 1,
-                             colClasses = "character",
-                             header = FALSE)[1, ])
-# inital run took 16 sec's of elapsed time, reduced to 0.25 sec's after
-# reading in only the 2880 rows needed for project
-#system.time({
-    data <- read.table(con, header = TRUE, sep = ";",
-                       skip = 66637, nrows = 2880)
-    names(data) <- make.names(headers) #, allow_ = FALSE)
-#})
-close(con)
+library(dplyr)
+
+
+readHouseholdPowerData <-
+    function(fileName, startRow, readCount) {
+        colClasses <- c("character", "character", rep("numeric", 7))
+        con <- file(fileName)
+        open(con)
+        headers <- unlist(read.table(con, sep = ";", nrows = 1,
+                                     colClasses = "character",
+                                     header = FALSE)[1, ])
+        data <- read.table(con, header = TRUE, sep = ";",
+                           skip = (startRow - 1), nrows = readCount)
+        names(data) <- make.names(headers) #, allow_ = FALSE)
+        close(con)
+        
+        return(data)
+    }
+
+# Read just the data from the data 2007-02-01 and 2007-02-02. This data starts
+# at row 66638 and continue for 2880 records
+data <- readHouseholdPowerData(fileName = "household_power_consumption.txt",
+                               startRow = 66638, readCount = 2880)
+hist(data$Global_active_power, main = "Global Active Power",
+     col = "red", xlab = "Global Active Power (kilowatts)")
+# create/write output png. Default size is 480 x 480 pixels, but be explicit
+dev.copy(png, file = "plot1.png", width = 480, height = 480, units = "px")
+dev.off()
